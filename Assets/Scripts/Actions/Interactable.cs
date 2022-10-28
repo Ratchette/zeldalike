cd17cbc4937410ac2af7fb46bcf7d57e2c528547
Jennifer Winer <jenn.winer@gmail.com>
2022-10-28 12:26:02 +0100

    Refactor sign into generic Interactable type

---
+++ Assets/Scripts/Actions/Interactable.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+    [SerializeField] private GameObject dialogBox;
+    [SerializeField] private TextMeshProUGUI dialogText;
+    [SerializeField][TextArea(3, 4)] private List<string> text;
+
+    private bool playerInRange = false;
+    private int activeText = -1;
+
+    public bool CanInteract() {
+        return (Input.GetButtonDown("interact") && playerInRange);
+    }
+
+    // Cycles through every text field 
+    public void Interact() {
+        if (activeText == -1) {
+            dialogBox.SetActive(true);
+            activeText++;
+            dialogText.SetText(text[activeText]);
+
+        } else if ((activeText + 1) >= text.Count) {
+            // We've cycled through all of the text, so close the dialog.
+            dialogBox.SetActive(false);
+            activeText = -1;
+
+        } else {
+            // Display the next section of text.
+            activeText++;
+            dialogText.SetText(text[activeText]);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            playerInRange = false;
+
+            // If the player walks away while the text is shown
+            if (dialogBox.activeInHierarchy && activeText != -1) {
+                dialogBox.SetActive(false);
+                activeText = -1;
+            }
+        }
+    }
+}
--- Assets/Scripts/Objects/Sign.cs
+++ Assets/Scripts/Objects/Sign.cs
@@ -5,38 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Sign : MonoBehaviour {
-    [SerializeField] private GameObject dialogBox;
-    [SerializeField] private TextMeshProUGUI dialogText;
-    [SerializeField] [TextArea(3, 4)] private string text;
-
-    private bool playerInRange;
+public class Sign : Interactable {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown("interact") && playerInRange) {
-            if (dialogBox.activeInHierarchy) {
-                dialogBox.SetActive(false);
-            } else {
-                dialogText.SetText(text);
-                dialogBox.SetActive(true);
-            }
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            playerInRange = true;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            playerInRange = false;
-
-            if(dialogBox.activeInHierarchy && dialogText.text.Equals(text)) {
-                dialogBox.SetActive(false);
-            }
+        if (CanInteract()) {
+            Interact();
         }
     }
 }
