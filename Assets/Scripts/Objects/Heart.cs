55086e89fc9caf1eb4c04abd2e54c15c8e889ec9
Jennifer Winer <jenn.winer@gmail.com>
2023-05-08 21:42:43 +0100

    Implement hearts in rough way

---
+++ Assets/Scripts/Objects/Heart.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour {
+
+    [SerializeField] private SignalSender healthSignal;
+
+    [SerializeField] private FloatValue heartContainers;
+    [SerializeField] private FloatValue playerHealth;
+
+    [SerializeField] float healthIncrease;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger) {
+            float maxHealth = heartContainers.runtimeValue * 4;
+            healthIncrease = Mathf.Min(healthIncrease, maxHealth - playerHealth.runtimeValue);
+
+            playerHealth.runtimeValue += healthIncrease;
+            healthSignal.Raise();
+
+            Destroy(this.gameObject);
+        }
+    }
+
+}
